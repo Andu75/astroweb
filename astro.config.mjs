055416589224b85dc9aeeb284bda87ac123abacc
STEFnr1 <83458848+STEFnr1@users.noreply.github.com>
2025-01-14 12:12:46 +0200

    uploaded site
    ACTUALLY uploaded the site this time, you heard me Andu?

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,3 +3,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({});
+import { defineConfig } from 'astro/config'
+
+export default defineConfig({
+  site: 'https://andu75.github.io',
+  base: 'my-astroweb',
+})
